fc19d48da5b63c4ede21016f63305c7c9ba37d03
AlexRakeem52 <gurutype.ai@gmail.com>
2025-05-25 18:01:00 -0500

    Update Results.js

--- components/Results.js
+++ components/Results.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import 'chart.js/auto';
-import '@/styles/globals.css';
+import '../styles/globals.css';
 
 export default function Results({ scores }) {
   const data = {
