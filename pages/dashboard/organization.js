5e448d06a64d1bb41a16670ad57c52b8000543db
AlexRakeem52 <gurutype.ai@gmail.com>
2025-06-23 02:50:03 -0500

    Update organization.js

--- pages/dashboard/organization.js
+++ pages/dashboard/organization.js
@@ -1,8 +1,5 @@
 import dynamic from 'next/dynamic';
-
-const OrganizationDashboard = dynamic(() => import('../../components/OrganizationDashboard'), {
-  ssr: false,
-});
+const OrganizationDashboard = dynamic(() => import('../../components/OrganizationDashboard'), { ssr: false });
 
 export default function OrganizationPage() {
   return <OrganizationDashboard />;
