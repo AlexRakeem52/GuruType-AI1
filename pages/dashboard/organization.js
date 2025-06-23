c884eb60672ba8dabb7742b8d76d5d8896972755
AlexRakeem52 <gurutype.ai@gmail.com>
2025-06-23 02:25:48 -0500

    Update organization.js

--- pages/dashboard/organization.js
+++ pages/dashboard/organization.js
@@ -1,9 +1,9 @@
 import dynamic from 'next/dynamic';
 
-const NoSSR = dynamic(() => import('../../components/OrganizationDashboard'), {
+const OrganizationDashboard = dynamic(() => import('../../components/OrganizationDashboard'), {
   ssr: false,
 });
 
-export default function OrganizationDashboardPage() {
-  return <NoSSR />;
+export default function OrganizationPage() {
+  return <OrganizationDashboard />;
     }
